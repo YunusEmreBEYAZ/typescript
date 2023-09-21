f3980cc1494aafdacb3e4b0d2e170097b2e2bc5d
Yunus Emre BEYAZ <yebeyaz@gmail.com>
2023-09-22 00:23:32 +0200

    started

---
+++ Project1/sandbox.ts
@@ -0,0 +1,13 @@
+const char = "mario";
+
+console.log(char);
+
+const inputs = document.querySelectorAll("input");
+
+console.log(inputs);
+
+
+inputs.forEach(input => {
+    console.log(input);
+
+})
