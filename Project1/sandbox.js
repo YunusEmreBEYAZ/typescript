c493e111d65aea27ffcf722ffc7aec974241fff3
Yunus Emre BEYAZ <yebeyaz@gmail.com>
2023-09-29 08:39:33 +0200

    explicit types

--- Project1/sandbox.js
+++ Project1/sandbox.js
@@ -1,41 +1,11 @@
-var char = "mario";
-console.log(char);
-var inputs = document.querySelectorAll("input");
-console.log(inputs);
-inputs.forEach(function (input) {
-    console.log(input);
-});
-//functions
-var circle = function (diameter) {
-    return diameter * Math.PI;
-};
-console.log(circle(5));
-//console.log(circle('Hello'));  We can not push number because initially we set  our function as number
+//EXPLICIT TYPES
+var character;
+var age;
+var isLoggedIn;
+// age ='Yunus' => this will give error because we assigned age as a number
 //ARRAYS
-var names = ['Yunus', 'Ozgur', 'Omer'];
-names.push('Emin');
-//names[0] = 5; //we can not change string to number
-console.log(names[0]);
-console.log(names);
-//names.push(5);  We can not push number because initially our array is string!
-// If it is mixed you can change them!
-var mixed = [1, 2, 3, 'Yunus', 'Emre'];
-mixed.push(150);
-// OBJECTS
-var person = {
-    name: 'Yunus',
-    age: 30
-};
-person.age = 55;
-// person.name = 30 // we cant change property type;
-// person.job = 'Web Developer' We can not add new properties !!
-// We can change object itself but not the type!
-person = {
-    name: 'Emre',
-    age: 20
-};
-//We can not change the initial structure(add or delete) !!
-person = {
-    name: 'Emre',
-    age: 20
-};
+var stringArray;
+var initialize = [];
+// In this cases we can only assign that 2 arrays string, but int the  first one we cannot use push. we get error if we try
+//stringArray.push('yunus'); => Uncaught TypeError: Cannot read properties of undefined (reading 'push')
+initialize.push('yunus');
--- Project1/sandbox.ts
+++ Project1/sandbox.ts
@@ -1,61 +1,17 @@
-const char = "mario";
 
-console.log(char);
+//EXPLICIT TYPES
+let character: string;
+let age: number;
+let isLoggedIn: boolean;
 
-const inputs = document.querySelectorAll("input");
-
-console.log(inputs);
-
-
-inputs.forEach(input => {
-    console.log(input);
-
-})
-//functions
-
-const circle = (diameter: number) => {
-    return diameter * Math.PI;
-}
-
-console.log(circle(5));
-//console.log(circle('Hello'));  We can not push number because initially we set  our function as number
-
+// age ='Yunus' => this will give error because we assigned age as a number
 
 //ARRAYS
-let names = ['Yunus', 'Ozgur', 'Omer'];
-names.push('Emin');
-//names[0] = 5; //we can not change string to number
-console.log(names[0]);
-console.log(names);
-//names.push(5);  We can not push number because initially our array is string!
+let stringArray: string[];
+let initialize: string[] = [];
 
-// If it is mixed you can change them!
+// In this cases we can only assign that 2 arrays string, but int the  first one we cannot use push. we get error if we try
 
-let mixed = [1, 2, 3, 'Yunus', 'Emre'];
-mixed.push(150);
+//stringArray.push('yunus'); => Uncaught TypeError: Cannot read properties of undefined (reading 'push')
 
-// OBJECTS
-
-let person = {
-    name: 'Yunus',
-    age: 30
-};
-
-person.age = 55;
-// person.name = 30 // we cant change property type;
-// person.job = 'Web Developer' We can not add new properties !!
-
-// We can change object itself but not the type!
-person = {
-    name: 'Emre',
-    age: 20
-}
-
-//We can not change the initial structure(add or delete) !!
-person = {
-    name: 'Emre',
-    age: 20,
-    //job: 'Web Developer'
-}
-
-
+initialize.push('yunus')
